f9e016e644012e59db6d3535753221c91c47f2a3
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-08 21:10:01 -0700

    prints programing is a block

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
 #includeb <stdio.h>
-/**
-* prints
-* programming
-* is like
-* building 
-* a multilingual
-* puzzle
-*/
 int main (void)
-	{
-		puts ("\"Programming is like building a multilingual puzzle");
-		return(0);
-	}
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
