da8515459a1c20258c4f12673c8dbc4f546551c8
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-12 15:40:00 -0700

    alphabetical order

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This code prints alphabets from a to z in lower case
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabbet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
