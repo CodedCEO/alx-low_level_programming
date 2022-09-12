1580e9a84bd867e625b2e7c5bfcef2954dc11940
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-12 14:28:34 -0700

    base 16 in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program prints all the numbers of base 16
+ * in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 'y'; y <= '9'; y++)
+	{
+	putchar(y + '0');
+	}
+for (y = 'y'; y	<= 'g'; y++)
+{
+	putchar(y);
+}
+putchar('\n);
+return (0);
+}
+
