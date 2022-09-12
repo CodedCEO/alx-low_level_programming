310d5da5db9a99d9d502cd7c7aae5c2cbd6ca006
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-12 16:12:21 -0700

    prints numbers less than 10 in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This prints all single digits number of base 10
+ * starting from 0 using one putchar and without declaring
+ * using char
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (i = 0; i < 10, i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
