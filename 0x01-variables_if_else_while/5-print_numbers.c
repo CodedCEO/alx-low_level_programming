6189ac8d31e3b31782038157e8db44dbc67ba95b
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-12 16:06:02 -0700

    ascending order of digits less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints all single digits up to 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
