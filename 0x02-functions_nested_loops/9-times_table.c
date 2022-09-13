b8475bc6c6d397c21893cdf3784e7e7b55e5f5ea
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-13 12:44:16 -0700

    initial

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,13 +20,11 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int temp = i * j;
-			
+			int temp = i * j;	
 			if (temp / 10)
 			{
 				int num1;
 				int num2;
-				
 				num1 = temp / 10;
 				num2 = temp % 10;
 				_putchar('0' + num1);
