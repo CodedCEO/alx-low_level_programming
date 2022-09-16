449aaa1b34b73583c06c1a0d8e554e5d3768428d
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-15 20:33:21 -0700

    main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,59 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - determines uppercase or not
+ * @c: character to determine
+ * Return: true or false
+ */
+int_isupper(int c);
+/**
+ * _isdigit - checks if it is a digit
+ * @c: character to check
+ * Return: returns boolean
+ */
+int_isdigit(int c);
+/**
+ * mul - multiplies integers
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: gives result
+ */
+int mul(int a, int b);
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+void print_numbers(void);
+/**
+ * print_most_numbers - print the numbers
+ *
+ */
+void print_most_numbers(void);
+/**
+ * more_numbers - prints the numbers
+ *
+ */
+void more_numbers(void);
+/**
+ * print_line - prints a line
+ * @n: integer to print a line
+ */
+void print_line(int n);
+/**
+ * prints_diagonal - prints diagonal
+ * @n: integer to see how many lines
+ */
+void print_diagonal(int n);
+/**
+ * print_square - prints a square
+ * @size: prints the size of the square
+ */
+void print_square(int size);
+/**
+ * print_triangle - prints triangle
+ * @size: size of a triangle
+ */
+void print_triangle(int size);
+
+#endif
