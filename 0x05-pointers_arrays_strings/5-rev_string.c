a56be46baed188ab9186edb4e604f9d0849b550c
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-19 13:32:30 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	int length;
 	int n;
 	int k;
-	
+
 	length = get_length(s);
 
 	for (n = 0, k = length - 1; !(n >= k); n++, k--)
@@ -24,4 +24,25 @@ void rev_string(char *s)
 		s[n] = s[k];
 		s[k] = count;
 	}
+}
+
+/**
+ * get_length - returns string length
+ * @s: string whose length is to be determined
+ *
+ * Return - length of string
+ */
+int get_length(char *s)
+{
+	int length;
+	int n;
+
+	length = 0;
+	n = 0;
+	while (s[n] != '\0')
+	{
+		++length;
+		n++;
+	}
+	return (length);
 }
