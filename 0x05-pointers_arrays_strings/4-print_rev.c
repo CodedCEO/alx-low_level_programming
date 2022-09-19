3addd26d9969884ba22b246b8e5c40a62f1d333c
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-19 12:59:17 -0700

    prints reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int get_length(char *);
+
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: String to be printed in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	length = get_length(s);
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
