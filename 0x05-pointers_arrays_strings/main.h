2a4e3d873ce977394746a184fccd6b9776100749
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-20 06:05:26 -0700

    heder

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
-int _putchar(char);
+#include <stdio.h>
+#include <string.h>
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
