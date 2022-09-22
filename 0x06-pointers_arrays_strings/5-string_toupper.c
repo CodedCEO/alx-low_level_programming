8d62b62628432e5de7d572e8e3a7fa5ec91affac
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-22 09:53:01 -0700

    changes lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,39 +2,21 @@
 
 /*
  * string_toupper - changes all lowercase letters of a string
- *
  * to uppercase
+ * @s: input string.
  *
- *    * @s: input string.
- *
- *     * Return: the pointer to dest.
- *
- *      */
-
-
+ * Return: the pointer to dest.
+ */
 
 char *string_toupper(char *s)
-
 {
+	int count = 0;
 
-		int count = 0;
-
-
-
-			while (*(s + count) != '\0')
-
-					{
-
-								if ((*(s + count) >= 97) && (*(s + count) <= 122))
-
-												*(s + count) = *(s + count) - 32;
-
-										count++;
-
-											}
-
-
-
-				return (s);
-
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+	return (s);
 }
