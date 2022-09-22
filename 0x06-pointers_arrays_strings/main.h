788ca2e0d4a7e04a057d0575f0d6eced3755b55e
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-09-22 06:24:12 -0700

    main file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -17,4 +15,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
