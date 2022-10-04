8de224cf9129f09cf7d2a4b27a03de301a74c1d5
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-10-04 05:48:16 -0700

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - prints at the end of the day
+ * my goal is to be the best hacker
+ *
+ * Return: void
+ */
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n
